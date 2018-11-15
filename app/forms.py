0801f8aba94167995049548b11a1d64712c29903
wizmariefa <mariecolee1@gmail.com>
2018-11-14 19:26:13 -0700

    need to remove extraneous labels from input list

--- app/forms.py
+++ app/forms.py
@@ -1,12 +1,27 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField, TextAreaField, BooleanField, validators
+from wtforms import IntegerField, StringField, SubmitField, BooleanField, validators
+from wtforms import FieldList
+from wtforms import FormField
+from wtforms_alchemy import PhoneNumberField
 from wtforms.validators import DataRequired
 
-class ContactForm(FlaskForm):
-    names = TextAreaField('Names of Participants', validators=[DataRequired()])
-    contact_info = TextAreaField('Phone Numbers', validators=[DataRequired()])
-    submit = SubmitField('Generate Secret Santa!')
 
-class SendForm(FlaskForm):
-    text = BooleanField('Send Via Text', validators=[validators.AnyOf([True, False])])
-    submit = SubmitField('Send em\' out!')
+class NumParticipantsForm(FlaskForm):
+    num_people = IntegerField('Number of Participants', validators=[])
+    submit = SubmitField('Input Names and Numbers')
+
+
+class PhoneEntryForm(FlaskForm):
+    phone_number = PhoneNumberField()
+
+
+class NameEntryForm(FlaskForm):
+    name_entry = StringField()
+
+
+class ContactForm(FlaskForm):
+    names = FieldList(FormField(NameEntryForm), min_entries=4)
+    contact_info = FieldList(FormField(PhoneEntryForm), min_entries=4)
+    text = BooleanField('Send Via Text', validators=[
+                        validators.AnyOf([True, False])])
+    submit = SubmitField('Generate Secret Santa!')
--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,7 @@
 from app import app
 from flask import render_template, flash, redirect
 from .santa_class import SantaGenerator
-from app.forms import ContactForm, SendForm
+from app.forms import ContactForm, NumParticipantsForm
 from .message_sender import MessageSender
 global names
 global contact_info
@@ -11,17 +11,13 @@ global ms
 @app.route('/', methods=['get', 'post'])
 @app.route('/index', methods=['get', 'post'])
 def index():
-    global names
-    global contact_info
-
-    contact_form = ContactForm()
-    if contact_form.validate_on_submit():
-        flash('Santa-list generated for {}, with contact {}'.format(contact_form.names.data,
-                                                                    contact_form.contact_info.data))
-        names = contact_form.names.data
-        contact_info = contact_form.contact_info.data
+    global num_ppl
+    ppl_form = NumParticipantsForm()
+    if ppl_form.validate_on_submit():
+        flash("Generating page for {} participants".format(ppl_form.num_people.data))
+        num_ppl = ppl_form.num_people.data
         return redirect('/santa')
-    return render_template('index.html', form=contact_form)
+    return render_template('index.html', form=ppl_form)
 
 
 @app.route('/santa', methods=['get', 'post'])
@@ -29,10 +25,20 @@ def santa():
     global names
     global contact_info
     global sg
-    send_form = SendForm()
-    if send_form.is_submitted():
-        flash('Sending via text?'.format(send_form.text.data))
-        am_i_texting = send_form.text.data
+    global num_ppl
+
+    contact_form = ContactForm()
+
+    if contact_form.is_submitted():
+        flash('Santa-list generated for {}, with contact {}'.format(contact_form.names.data,
+                                                                    contact_form.contact_info.data))
+        names = contact_form.names.data
+        contact_info = contact_form.contact_info.data
+
+        print(names)
+        print(contact_info)
+        flash('Sending via text?'.format(contact_form.text.data))
+        am_i_texting = contact_form.text.data
 
         sg = SantaGenerator(names)
 
@@ -40,8 +46,8 @@ def santa():
             return redirect('/sent')
         else:
             return redirect('/assignments')
-            
-    return render_template('santa.html', form=send_form)
+
+    return render_template('santa.html', form=contact_form)
 
 
 @app.route('/assignments')
